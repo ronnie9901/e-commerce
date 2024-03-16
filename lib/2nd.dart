f143cec278c39d031f450bea517f9361f7cd0d7d
Rohan Nayak <nayakrohan9901@gmail.com>
2024-03-16 11:30:58 +0530

    10-03-2024

---
+++ lib/2nd.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          centerTitle: true,
+          leading: Icon(Icons.menu),
+          title: Text('Flutter App',style: TextStyle(color: Colors.white),),
+          actions: [Icon(Icons.more_vert), ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                '  Red & White Group of institude:\n'
+                    'One Step in Changing Education Chain...',
+                style: TextStyle(fontSize: 18,color: Colors.red),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
